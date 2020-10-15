6b2b5620378c5864094fd8822b576de670e1804d
LTstrange <835422774@qq.com>
2020-10-15 20:01:43 +0800

    使用python的property修饰器。并修复了获胜检测中，黑白棋显示混淆的问题。

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ if __name__ == '__main__':
         print(board)
         print(f"{['白棋', '黑棋'][player]}回合:{[chr(9678), chr(9679)][player]}")
         if player:
-            step = ai.take_action(board.get_board())
+            step = ai.take_action(board.board)
             # step = input("enter you step, like H-8:")
             # step = step.split('-')
             board.put_stone(step[0].upper(), int(step[1]), int(player))
         else:
-            step = ai.take_action(board.get_board())
+            step = ai.take_action(board.board)
             # step = input("enter you step, like H-8:")
             # step = step.split('-')
             board.put_stone(step[0].upper(), int(step[1]), int(player))
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         player = not player
     print(board)
     board.check_win()
-    print(board.get_pre_step())
+    print(board.previews_step)
     with open('chess_book_temp.pkl', 'wb') as file:
-        pickle.dump(board.get_chess_book(), file)
+        pickle.dump(board.chess_book, file)
 
